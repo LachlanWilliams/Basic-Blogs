e0ed8d27852def52a26f93a7bbf841eb159c10f6
Lachlan Williams <lwil0039@student.monash.edu>
2023-06-15 19:06:14 +1000

    Board class

--- display.js
+++ display.js
@@ -4,9 +4,13 @@
 const box = document.createElement('div');
 
 // Set the desired styles for the box
-box.style.width = '100px';
-box.style.height = '100px';
+box.style.width = '80px';
+box.style.height = '80px';
 box.style.backgroundColor = 'black';
+box.style.margin = 'auto'; // Center horizontally
+box.style.position = 'relative';
+box.style.top = '50%';
+
 
 // Get the chessboard element by its ID
 const chessboard = document.getElementById('chessboard');
