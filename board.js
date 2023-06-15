630fb4c085e1e745dbd9de135a8540238f0448e2
Lachlan Williams <lwil0039@student.monash.edu>
2023-06-15 19:11:49 +1000

    create board method

--- board.js
+++ board.js
@@ -1,4 +1,13 @@
 class Board {
     constructor(){
+        this.board = this.createBoard
+    }
+
+    createBoard() {
+        const board = [];
+        for (let i = 0; i < 8; i++) {
+          board.push(Array(8).fill(0));
+        }
+        return board;
     }
 }
