9e68d18c8ae3abcb66b3325dc7b66177e4880d97
Lachlan Williams <lwil0039@student.monash.edu>
2023-06-18 14:01:06 +1000

    url switcj

--- server.js
+++ server.js
@@ -8,9 +8,21 @@ const server = http.createServer((req,res) => {
     // This sets the header content type 
     res.setHeader('Content-Type', 'text/html');
 
+    let path = './pages/'
+
+    // add any switch cases for each html file
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            break;
+        default:
+            path += '404.html';
+            break;
+    }
+
     // send an html file 
     // fs (file system) creates a stream
-    fs.readFile('./pages/index.html', (err, data) => {
+    fs.readFile(path, (err, data) => {
         if(err){
             console.log(err);
         }
