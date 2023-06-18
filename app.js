b1d582a315d5bd4ec96a0335f90a7acba1cfad3e
Lachlan Williams <lwil0039@student.monash.edu>
2023-06-18 15:21:29 +1000

    app.js handles redirect

--- app.js
+++ app.js
@@ -6,16 +6,19 @@ const app = express();
 // listen for requests
 app.listen(3000);
 
+// takes user to main page 
 app.get('/', (req,res) => {
-
     //res.send('<p>home page</p>');
-    res.sendFile('./pages/index.html', {root: __dirname})
-
+    res.sendFile('./pages/index.html', {root: __dirname});
 });
 
+// takes user to about page
 app.get('/about', (req,res) => {
-
     //res.send('<p>about page</p>');
-    res.sendFile('./pages/about.html', {root: __dirname})
-    
+    res.sendFile('./pages/about.html', {root: __dirname});
 });
+
+// redirect function
+app.get('/about-me', (req,res) => {
+    res.redirect('/about');
+})
