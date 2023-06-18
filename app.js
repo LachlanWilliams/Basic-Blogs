bcea86c8954417ac9c87ac139261dadd66ab4c44
Lachlan Williams <lwil0039@student.monash.edu>
2023-06-18 15:14:17 +1000

    app.js loads html pages

--- app.js
+++ app.js
@@ -5,3 +5,17 @@ const app = express();
 
 // listen for requests
 app.listen(3000);
+
+app.get('/', (req,res) => {
+
+    //res.send('<p>home page</p>');
+    res.sendFile('./pages/index.html', {root: __dirname})
+
+});
+
+app.get('/about', (req,res) => {
+
+    //res.send('<p>about page</p>');
+    res.sendFile('./pages/about.html', {root: __dirname})
+    
+});
